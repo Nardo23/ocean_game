6ef773b98555696b4e31aa92c65e7af090f358e9
pee <leonardo.price@gmail.com>
2021-03-25 13:33:33 -0400

    waterfalls are doors over time

--- ocean game/Assets/door.cs
+++ ocean game/Assets/door.cs
@@ -10,20 +10,53 @@ public class door : MonoBehaviour
     private Vector3 NewExit;
     public Player playerScript;
     public bool changeWorld;
+    public bool lerpDoor = false;
+    private bool startLerp = false;
+
+    private float startTime;
+    private float journeyLength;
 
     // Start is called before the first frame update
     void Start()
     {
-        NewExit = new Vector3 (ExitOffset.x+Exit.position.x, ExitOffset.y+ Exit.position.y, 0f); 
+        NewExit = new Vector3 (ExitOffset.x+Exit.position.x, ExitOffset.y+ Exit.position.y, 0f);
+        journeyLength = Vector3.Distance(Playerposition.position, NewExit);
+        
     }
 
-    
+    private void Update()
+    {
+        if (startLerp)
+        {
+            float distCovered = (Time.time-startTime) *4;
+            float fractionOfJourney = distCovered / journeyLength;
+            Playerposition.position = Vector3.Lerp(Playerposition.position, NewExit, fractionOfJourney);
+        }
+        if(Playerposition.position == NewExit)
+        {
+            startLerp = false;
+            playerScript.canMove = true;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             Debug.Log("Piss");
-            Playerposition.position = NewExit;
+            if (lerpDoor)
+            {
+                startTime = Time.time;
+                playerScript.canMove = false;
+                startLerp = true;
+
+            }
+            else
+            {
+                Playerposition.position = NewExit;
+            }
+            
+            
             if (changeWorld)
             {
                 playerScript.SwapWorld();
