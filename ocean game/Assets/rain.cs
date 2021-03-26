a73530fb27a570e8880051f97ad921f58ebf3862
pee <leonardo.price@gmail.com>
2021-03-26 12:13:53 -0400

    rain

---
+++ ocean game/Assets/rain.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class rain : MonoBehaviour
+{
+    public Animator weatherAnim;
+    public bool raining = false;
+    private ParticleSystem rainParticles;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rainParticles = GetComponent<ParticleSystem>();
+        rainParticles.Stop();
+
+
+    }
+    void SetRain()
+    {
+        weatherAnim.SetBool("rain", true);
+        rainParticles.Play();
+
+    }
+    void EndRain()
+    {
+        weatherAnim.SetBool("rain", false);
+        rainParticles.Stop();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (raining)
+        {
+            SetRain();
+        }
+        else
+        {
+            EndRain();
+        }
+    }
+}
