20b2d5d606381074043fad28d1d0cc7b55ea2684
pee <leonardo.price@gmail.com>
2021-05-27 21:13:10 -0400

    sea platform

---
+++ ocean game/Assets/SeaPlatform.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaPlatform : MonoBehaviour
+{
+    EdgeCollider2D edge;
+    CapsuleCollider2D cap;
+    public GameObject floorCols;
+    public GameObject floor;
+    public SpriteRenderer sprt;
+    public SpriteRenderer sprt2;
+    public SpriteRenderer sprt3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        floorCols.SetActive(false);
+        edge = GetComponent<EdgeCollider2D>();
+        cap = GetComponent<CapsuleCollider2D>();
+        floorCols.SetActive(false);
+        edge.enabled = false;
+        cap.enabled = true;
+        sprt.sortingLayerName = "ui";
+        sprt2.sortingLayerName = "ui";
+        sprt3.sortingLayerName = "ui";
+        GetComponent<SpriteRenderer>().sortingLayerName = "Default";
+        GetComponent<ysort>().enabled = true;
+        childToggle(false);
+
+    }
+
+
+   void childToggle(bool onOff)
+    {
+        Transform[] allChildren = floor.GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            if (child.GetComponent<CircleCollider2D>() != null)
+            {
+                child.GetComponent<CircleCollider2D>().enabled = onOff;
+            }
+            if (child.GetComponent<BoxCollider2D>() != null)
+            {
+                child.GetComponent<BoxCollider2D>().enabled = onOff;
+            }
+            if (child.GetComponent<SpriteRenderer>() != null)
+            {
+                if (!onOff)
+                {
+                    child.GetComponent<SpriteRenderer>().sortingLayerName = "ui";
+                    if(child.GetComponent<ysort>() != null)
+                    {
+                        child.GetComponent<ysort>().enabled = false;
+                        child.GetComponent<SpriteRenderer>().sortingOrder = 0;
+                    }
+                }
+                else
+                {
+                    child.GetComponent<SpriteRenderer>().sortingLayerName = "Default";
+                    if (child.GetComponent<ysort>() != null)
+                    {
+                        child.GetComponent<ysort>().enabled = true;
+                        
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            floorCols.SetActive(true);
+            edge.enabled = true;
+            cap.enabled = false;
+            sprt.sortingLayerName = "Default";
+            sprt2.sortingLayerName = "Default";
+            sprt3.sortingLayerName = "Default";
+            GetComponent<SpriteRenderer>().sortingLayerName = "tile";
+            GetComponent<ysort>().enabled = false;
+            GetComponent<SpriteRenderer>().sortingOrder = 0;
+            childToggle(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            floorCols.SetActive(false);
+            edge.enabled = false;
+            cap.enabled = true;
+            sprt.sortingLayerName = "ui";
+            sprt2.sortingLayerName = "ui";
+            sprt3.sortingLayerName = "ui";
+            GetComponent<SpriteRenderer>().sortingLayerName = "Default";
+            GetComponent<ysort>().enabled = true;
+            childToggle(false);
+
+        }
+    }
+
+}
