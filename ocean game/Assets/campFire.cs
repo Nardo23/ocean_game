fe05ac50838431d255742bd11f4a5c8f0f6f35fe
pee <leonardo.price@gmail.com>
2021-06-02 21:06:12 -0400

    light and put out campfires

---
+++ ocean game/Assets/campFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class campFire : MonoBehaviour
+{
+    public GameObject fire;
+    public bool canToggle = true;
+    public AudioClip fireStartSound;
+    AudioSource sor;
+
+    private void Start()
+    {
+        sor = GetComponent<AudioSource>();
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            if(canToggle)
+                fire.SetActive(!fire.activeSelf);
+
+            if (fire.activeSelf == true)
+            {
+                sor.PlayOneShot(fireStartSound);
+
+            }
+        }
+    }
+
+}
