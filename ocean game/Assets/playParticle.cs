a3166675872ec6ecd98bb2006eb4367dd2213607
pee <leonardo.price@gmail.com>
2021-06-17 15:27:29 -0400

    big mushrooms

---
+++ ocean game/Assets/playParticle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playParticle : MonoBehaviour
+{
+
+    public ParticleSystem particle;
+
+    public void ParticlesPlay()
+    {
+        particle.Play();
+    }
+}
---
+++ ocean game/Assets/spores.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spores : MonoBehaviour
+{
+    public GameObject sporesObj;
+
+    private void Start()
+    {
+        sporesObj.SetActive(false);
+    }
+
+    public void SporesPlay()
+    {
+        sporesObj.SetActive(true);
+    }
+
+    public void SporesStop()
+    {
+        sporesObj.SetActive(false);
+    }
+
+
+}
