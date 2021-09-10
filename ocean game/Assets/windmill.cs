ba1e199fa6b3a5fb05ce3af7ab9ac7926321c124
pee <leonardo.price@gmail.com>
2021-09-10 18:22:46 -0400

    windmill skull debris

---
+++ ocean game/Assets/windmill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windmill : MonoBehaviour
+{
+    public Player playerScript;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerScript.windSpeed <= .2f)
+        {
+            anim.SetTrigger("stop");
+            
+        }
+        else
+        {
+            anim.SetTrigger("go");
+        }
+    }
+}
