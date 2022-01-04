c929f67bb08073785f72f23b504bc22f2bc87e52
pee <leonardo.price@gmail.com>
2022-01-04 18:06:20 -0500

    more wind shaders

--- ocean game/Assets/windShaders.cs
+++ ocean game/Assets/windShaders.cs
@@ -6,17 +6,37 @@ public class windShaders : MonoBehaviour
 {
 
     public Material[] windMats;
+    public GameObject[] pflags;
     
+    bool started = false;
+    public void Start()
+    {
+        if (!started)
+        {
+            startStuff();
+            started = true;
+        }
+
+        
 
 
-  
+    }
 
+    void startStuff()
+    {
+        pflags = GameObject.FindGameObjectsWithTag("pflag");
+        
+    }
 
 
     public void changeWinds(Vector2 direction, float speed)
     {
-
-        foreach(var material in windMats)
+        if (!started)
+        {
+            startStuff();
+            started = true;
+        }
+        foreach (var material in windMats)
         {
             
 
@@ -37,6 +57,40 @@ public class windShaders : MonoBehaviour
 
 
         }
+        
+        foreach(GameObject obj in pflags)
+        {
+            Debug.Log("pflgas");
+            if(obj.GetComponent<Animator>() != null)
+            {
+                
+                if(direction.x >= 0)
+                {
+                    obj.transform.localScale = new Vector3(-1, 1, 1);   
+                }
+                if (direction.x < 0)
+                {
+                    obj.transform.localScale = new Vector3(1, 1, 1);
+                }
+                if (speed > .5)
+                {
+                    obj.GetComponent<Animator>().Play("Base Layer.pflag fast", 0, Random.Range(0f,.2f));
+                }
+                else if (speed > 0)
+                {
+                    obj.GetComponent<Animator>().Play("Base Layer.pflagslow", 0, Random.Range(0f, .2f));
+                }
+                else
+                {
+                    obj.GetComponent<Animator>().Play("Base Layer.pflagidle", 0, Random.Range(0f, .2f));
+                }
+            }
+
+            
+        }
+
+
+
 
     }
 
