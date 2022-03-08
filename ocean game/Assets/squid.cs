29e48a65499143daf4c7d383da0e74826ab2ab39
pee <leonardo.price@gmail.com>
2022-03-08 15:50:14 -0500

    scenes for trailer and giant squid!

---
+++ ocean game/Assets/squid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class squid : MonoBehaviour
+{
+    [SerializeField]
+    Animator[] tentacle;
+    int count =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tentacleOff();
+    }
+
+  
+    void breach()
+    {
+        tentacle[count].gameObject.SetActive(true);
+        count++;
+    }
+    void resetCount()
+    {
+        count = 0;
+    }
+
+    void retract()
+    {
+        tentacle[count].SetTrigger("retract");
+        count++;
+    }
+
+    void tentacleOff()
+    {
+        foreach (Animator a in tentacle)
+        {
+            a.gameObject.SetActive(false);
+        }
+    }
+
+
+}
