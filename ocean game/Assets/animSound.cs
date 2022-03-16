06e224420858aedef089b4e399b45cbebf9089bb
pee <leonardo.price@gmail.com>
2022-03-16 16:41:40 -0400

    penguins

--- ocean game/Assets/animSound.cs
+++ ocean game/Assets/animSound.cs
@@ -7,13 +7,23 @@ public class animSound : MonoBehaviour
     public AudioClip clip1;
     public AudioSource sor;
     public AudioClip clip2;
-
+    [SerializeField]
+    AudioClip[] clips;
     public void playClip1()
     {
+        sor.pitch = 1;
         sor.PlayOneShot(clip1);
     }
     public void playClip2()
     {
+        sor.pitch = 1;
         sor.PlayOneShot(clip2);
     }
+
+    public void PlayRandomClip()
+    {
+        sor.pitch = Random.Range(.8f, 1.2f);
+        sor.PlayOneShot(clips[Random.Range(0, clips.Length)], .3f);
+    }
+
 }
